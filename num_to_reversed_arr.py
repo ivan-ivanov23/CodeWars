25575e7b9ead15e5f6a66d102623f0655c5cc242
Ivan <iai5555@abv.bg>
2024-08-29 16:13:50 +0300

    add another solution to last kata

--- num_to_reversed_arr.py
+++ num_to_reversed_arr.py
@@ -11,6 +11,16 @@ def digitize(n):
     x = [int(i) for i in n]
     return x
 
+
+# OR
+def digitize2(n):
+    result = [int(x) for a,x in enumerate(str(n))]
+    result.reverse()
+    return result
+
 # Test Cases
 print(digitize(35231)) # [1,3,2,5,3]
 print(digitize(348597)) # [7,9,5,8,4,3]
+print(" ")
+print(digitize2(35231)) # [1,3,2,5,3]
+print(digitize2(348597)) # [7,9,5,8,4,3]
