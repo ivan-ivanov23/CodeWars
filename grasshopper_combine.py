8ba0e50fa27d557f61729f38dbe48274bd5a39bd
Ivan <iai5555@abv.bg>
2024-09-12 11:02:44 +0300

    add solution 2 to grasshopper kata

--- grasshopper_combine.py
+++ grasshopper_combine.py
@@ -5,9 +5,19 @@ The function should return the full name.
 combine_names('James', 'Stevens') -> 'James Stevens'
 """
 
+# Solution 1
 def combine_names1(f_name, l_name):
     return f_name + " " + l_name
 
 # Test the function
 print(combine_names1("John", "Star")) # 'John Star'
 print(combine_names1("Ana", "Black")) # 'Ana Black'
+
+
+# Solution 2
+def combine_names2(*args):
+    return ' '.join(args)
+
+# Test the function
+print(combine_names2("John", "Star")) # 'John Star'
+print(combine_names2("Ana", "Black")) # 'Ana Black'
